e598800ec23c5c5c7e57bbce469cb96c11a07591
Vinnu Cho <vinnuleo109@gmail.com>
2019-03-16 15:35:42 +0530

    lib added

---
+++ loadingwait/src/main/java/com/svcodelabs/loadingwait/LoadingDialog.kt
@@ -0,0 +1,43 @@
+package com.svcodelabs.loadingwait
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.view.Window
+import android.widget.ImageView
+import androidx.annotation.LayoutRes
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+
+class LoadingDialog(private val context: Context) {
+
+    private fun buildDialogView(@LayoutRes layout: Int): Dialog {
+        val dialog = Dialog(context)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(layout)
+        dialog.setCancelable(false)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        return dialog
+    }
+
+    fun progressDialog(): Dialog {
+        val dialog = buildDialogView(R.layout.dialog_layout)
+
+        val avd = AnimatedVectorDrawableCompat.create(context, R.drawable.avd_dialog_img)
+        val iv = (dialog.findViewById<ImageView>(R.id.img)).apply {
+            setImageDrawable(avd)
+        }
+        avd?.registerAnimationCallback(object : Animatable2Compat.AnimationCallback() {
+            override fun onAnimationEnd(drawable: Drawable?) {
+                iv.post { avd.start() }
+            }
+        })
+        avd?.start()
+
+        return dialog
+    }
+
+}
